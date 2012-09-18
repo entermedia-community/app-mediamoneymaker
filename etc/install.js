dbf6b85cb552eaee53824f72590ec51b7b6738b6
Ian Miller <ian@ijsolutions.ca>
2012-09-18 13:45:20 -0400

    Fixed up some stuff

--- etc/install.js
+++ etc/install.js
@@ -42,10 +42,10 @@ files.copyFiles( tmp + "/WEB-INF/base/store", root + "/WEB-INF/base/store");
 
 
 log.add("5. UPGRADE CART FILES");
-files.deleteAll( root + "/media/catalogs/store");
-files.copyFiles( tmp + "/media/catalogs/store", root + "/media/catalogs/store");
+files.deleteAll( root + "/WEB-INF/base/media/catalogs/store");
+files.copyFiles( tmp + "/media/catalogs/store", root + "/WEB-INF/base/media/catalogs/store");
 
-files.deleteAll( root + "/ecommerce");
+files.deleteAll( root + "/WEB-INF/base/ecommerce");
 files.copyFiles( tmp + "/ecommerce", root + "/WEB-INF/base/ecommerce");
 
 
