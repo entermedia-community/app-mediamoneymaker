1d1ee471c3a1150180013c087369f5100427b691
shawn <shawn@netevolved.com>
2013-09-16 14:43:23 -0400

    Unit test for recurring orders

--- test/com/openedit/store/gateway/FreshbookTest.java
+++ test/com/openedit/store/gateway/FreshbookTest.java
@@ -3,10 +3,18 @@
  */
 package com.openedit.store.gateway;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
+import org.openedit.Data;
 import org.openedit.cart.freshbooks.FreshbooksStatus;
 import org.openedit.cart.freshbooks.FreshbooksManager;
+import org.openedit.data.Searcher;
+import org.openedit.data.SearcherManager;
+import org.openedit.entermedia.MediaArchive;
 import org.openedit.money.Money;
 import org.openedit.store.Cart;
 import org.openedit.store.CartItem;
@@ -44,8 +52,8 @@ public class FreshbookTest extends StoreTestCase {
 	 */
 	public void xxtestCreateCustomer() throws Exception {
 		FreshbooksManager util = new FreshbooksManager();
-		util.setToken("1486563a14b69f71a3ab62d2f9851ec6");
-		util.setUrl("https://shawnbest-billing.freshbooks.com/api/2.1/xml-in");
+		util.setToken("627d27a09019099d6c8b0e989c1ba8f0");
+		util.setUrl("https://learningevolved.freshbooks.com/api/2.1/xml-in");
 		
 		Customer customer = new Customer(new FileSystemUser());
 		customer.getUser().setId("testuser");
@@ -55,16 +63,15 @@ public class FreshbookTest extends StoreTestCase {
 
 		Address address = new Address();
 		address.setAddress1("123 Anywhere St.");
-		address.setState("Ontario");//Propercase works
+		address.setState("Ontario");//Propercase!
 		address.setCity("Collingwood");
-		address.setCountry("Canada");//Canada CANADA CAN can
+		address.setCountry("Canada");//Propercase!
 		address.setZipCode("L9Y 3T7");
 		
 		util.createCustmer(customer, address, null);
 	}
 	
 	public void testProcessInvoice() throws Exception {
-//		String clientid = "34008";
 		
 		Customer customer = new Customer(new FileSystemUser());
 		customer.getUser().setId("testuser1222");
@@ -74,31 +81,15 @@ public class FreshbookTest extends StoreTestCase {
 
 		Address address = new Address();
 		address.setAddress1("123 Anywhere St.");
-		address.setState("Ontario");//Propercase works
+		address.setState("Ontario");//Propercase!
 		address.setCity("Collingwood");
-		address.setCountry("Canada");//Propercase works
+		address.setCountry("Canada");//Propercase!
 		address.setZipCode("L9Y 3T7");
 		
-		customer.setBillingAddress(address);
+		customer.setBillingAddress(address);//add address to customer as a billing address
 		
-//		customer.getUser().setProperty("freshbooksid", clientid);
-		
-		// createRecurring(Order inOrder,FreshbookInstructions inStructions)
-		FreshbooksManager util = new FreshbooksManager();
-		util.setToken("1486563a14b69f71a3ab62d2f9851ec6");
-		util.setUrl("https://shawnbest-billing.freshbooks.com/api/2.1/xml-in");
-		util.setGateway("beanstream");
-		
-		FreshbooksStatus inStatus = new FreshbooksStatus();
-	    inStatus.setSendEmail("1");//send email 
-	    inStatus.setSendSnailMail("0");// don't send post mail
-	    inStatus.setBlocking(true);// force it to continue until finished
-	    inStatus.setDelayBetweenQueries(100);// 100ms between repeated queries (i.e., querying the status of an invoice)
-	    inStatus.setMaximumQueryRepeat(25);// perform only 25 queries at most
-	    
-	    inStatus.setFrequency("monthly");//kloog
-	    
-	    Store store = getStore();
+		//setup order
+		Store store = getStore();
 		CartModule cartModule = (CartModule) getFixture().getModuleManager().getModule("CartModule");
 
 		WebPageRequest context = getFixture().createPageRequest();
@@ -106,7 +97,38 @@ public class FreshbookTest extends StoreTestCase {
 		cart.setCustomer(customer);
 		
 		cart.addItem(createCheapToyCartItem());//add non-recurring
-		cart.addItem(createRecurringCartItem());//add recurring
+		
+		
+		CartItem item1 = createRecurringCartItem("Item 1",12.00);
+		CartItem item2 = createRecurringCartItem("Item 2",4.00);
+		CartItem item3 = createRecurringCartItem("Item 3",6.00);
+
+		//TODO in the UI:
+		//frequency and occurrences have to be added dynamically 
+		//ie., result of form submit
+		//if (item.getProduct().get("recurring") && item.getProduct().get("allowspartialpayments"))
+		//	display frequency and occurrences options
+		//on submit, before order is processed, 
+		// go through and find those fields and add them to the 
+		// cart items
+		
+		item1.setProperty("frequency", "2 weeks");
+		item1.setProperty("occurrences", "12");
+		
+		item2.setProperty("frequency", "monthly");
+		item2.setProperty("occurrences", "4");
+		
+		item3.setProperty("frequency", "monthly");
+		item3.setProperty("occurrences", "6");
+		cart.addItem(item1);
+		cart.addItem(item2);
+		cart.addItem(item3);
+		
+		
+		/*
+		 * String frequency = item.get("frequency");
+				String occurrence = item.get("occurrences");
+		 */
 		
 		Order order = store.getOrderGenerator().createNewOrder(store, cart);
 		order.setProperty("notes", "This is a note");
@@ -116,15 +138,23 @@ public class FreshbookTest extends StoreTestCase {
 		type.setName("Visa");
 		paymentMethod.setCreditCardType(type);
 		paymentMethod.setCardNumber("4030000010001234");//beanstream test credit card#
+		//declined: 4003050500040005
+		//approved: 4030000010001234
 		paymentMethod.setCardVerificationCode("123");
 		paymentMethod.setExpirationMonth(10);
-		paymentMethod.setExpirationYear(2015);
+		paymentMethod.setExpirationYear(2014);
 		
 		order.setPaymentMethod(paymentMethod);
+		
+		FreshbooksManager util = (FreshbooksManager) getFixture().getModuleManager().getBean(getStore().getCatalogId(), "freshbooksManager");
+		util.setToken("627d27a09019099d6c8b0e989c1ba8f0");
+		util.setUrl("https://learningevolved.freshbooks.com/api/2.1/xml-in");
+		util.setGateway("beanstream");
+		
+		FreshbooksStatus status = new FreshbooksStatus();
+	    util.processOrder(order, status);
 	    
-	    util.processOrder(order, inStatus);
-		
-		
+	    System.out.println(status.toString());
 		
 	}
 
@@ -133,8 +163,8 @@ public class FreshbookTest extends StoreTestCase {
 	 */
 	public void xxtestListClients() throws Exception {
 		FreshbooksManager util = new FreshbooksManager();
-		util.setToken("1486563a14b69f71a3ab62d2f9851ec6");
-		util.setUrl("https://shawnbest-billing.freshbooks.com/api/2.1/xml-in");
+		util.setToken("627d27a09019099d6c8b0e989c1ba8f0");
+		util.setUrl("https://learningevolved.freshbooks.com/api/2.1/xml-in");
 		Element list = util.getClientList();
 		assertNotNull(list);
 		System.out.println(list.asXML());
@@ -143,8 +173,8 @@ public class FreshbookTest extends StoreTestCase {
 	
 	public void xxtestListItems() throws Exception {
 		FreshbooksManager util = new FreshbooksManager();
-		util.setToken("1486563a14b69f71a3ab62d2f9851ec6");
-		util.setUrl("https://shawnbest-billing.freshbooks.com/api/2.1/xml-in");
+		util.setToken("627d27a09019099d6c8b0e989c1ba8f0");
+		util.setUrl("https://learningevolved.freshbooks.com/api/2.1/xml-in");
 		Element list = util.getItemList();
 		assertNotNull(list);
 		System.out.println(list.asXML());
@@ -153,8 +183,8 @@ public class FreshbookTest extends StoreTestCase {
 	
 	public void xxtestGatewayList() throws Exception {
 		FreshbooksManager util = new FreshbooksManager();
-		util.setToken("1486563a14b69f71a3ab62d2f9851ec6");
-		util.setUrl("https://shawnbest-billing.freshbooks.com/api/2.1/xml-in");
+		util.setToken("627d27a09019099d6c8b0e989c1ba8f0");
+		util.setUrl("https://learningevolved.freshbooks.com/api/2.1/xml-in");
 		Element list = util.getGatewayList();
 		assertNotNull(list);
 		System.out.println(list.asXML());
@@ -163,8 +193,8 @@ public class FreshbookTest extends StoreTestCase {
 	
 	public void xxtestCreateInvoice() throws Exception {
 		FreshbooksManager util = new FreshbooksManager();
-		util.setToken("1486563a14b69f71a3ab62d2f9851ec6");
-		util.setUrl("https://shawnbest-billing.freshbooks.com/api/2.1/xml-in");
+		util.setToken("627d27a09019099d6c8b0e989c1ba8f0");
+		util.setUrl("https://learningevolved.freshbooks.com/api/2.1/xml-in");
 		
 		Element root = DocumentHelper.createElement("request");
 		root.addAttribute("method", "invoice.create");
