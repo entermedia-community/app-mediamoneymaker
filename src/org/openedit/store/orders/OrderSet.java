d942e7229728400ac1599f8bc00f399dd78cd9e5
Peter Floyd <peterjfloyd@gmail.com>
2013-10-21 11:46:55 -0400

    OrderSet Changes

--- src/org/openedit/store/orders/OrderSet.java
+++ src/org/openedit/store/orders/OrderSet.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
+import org.openedit.data.Searcher;
+import org.openedit.data.SearcherManager;
 import org.openedit.money.Money;
 import org.openedit.store.Cart;
 import org.openedit.store.CartItem;
@@ -20,6 +22,7 @@ import com.openedit.OpenEditException;
 public class OrderSet {
 
 	protected List<Order> fieldOrders;
+	protected List<Order> fieldRemovedOrders;
 
 	public List<Order> getOrders() {
 		if (fieldOrders == null) {
@@ -28,17 +31,43 @@ public class OrderSet {
 		return fieldOrders;
 	}
 
+	public List<Order> getRemovedOrders() {
+		if (fieldRemovedOrders == null) {
+			fieldRemovedOrders = new ArrayList<Order>();
+		}
+		return fieldRemovedOrders;
+	}
+
 	public void setOrders(List<Order> inOrders) {
 		fieldOrders = inOrders;
+	}
+	
+	public void setRemovedOrders(List<Order> inOrders) {
+		fieldRemovedOrders = inOrders;
 	}
 	
 	public void addOrder(Order inOrder){
 		getOrders().add(inOrder);
 	}
 	
+	public void addRemovedOrder(Order inOrder){
+		getRemovedOrders().add(inOrder);
+	}
+	
 	public Order getOrder(String inId) {
 		Order order = null;
 		for (Iterator<Order> iterator = getOrders().iterator(); iterator.hasNext();) {
+			Order newOrder = iterator.next();
+			if (newOrder.getId().equals(inId)) {
+				order = newOrder;
+			}
+		}
+		return order;
+	}
+	
+	public Order getRemovedOrder(String inId) {
+		Order order = null;
+		for (Iterator<Order> iterator = getRemovedOrders().iterator(); iterator.hasNext();) {
 			Order newOrder = iterator.next();
 			if (newOrder.getId().equals(inId)) {
 				order = newOrder;
@@ -61,7 +90,7 @@ public class OrderSet {
 		Money money = new Money();
 		for (Iterator<Order> iterator = getOrders().iterator(); iterator.hasNext();) {
 			Order order = iterator.next();
-			Money total = (Money) order.getTax(); 
+			Money total = (Money) order.getTax();
 			money = money.add(total);
 		}
 		return money;
@@ -191,10 +220,11 @@ public class OrderSet {
 			Cart cart = inOrder.getCart();
 			cart.setStore(store);
 			cart.setItems(inOrder.getItems());
+			inOrder.setSubTotal(cart.getSubTotal());
 	
 			Customer customer = inOrder.getCustomer();
 			cart.setCustomer(customer);
-			
+
 			if (customer.getShippingAddress().getState().equals("NF")) {
 				customer.getShippingAddress().setState("NL");
 			}
@@ -208,10 +238,13 @@ public class OrderSet {
 				taxrates = cart.getStore().getTaxRatesFor(
 						customer.getBillingAddress().getState());
 			}
-			customer.setTaxRates(taxrates);
 			
+			if (taxrates == null || taxrates.size()==0) {
+				throw new OpenEditException("Taxrates is null");
+			}
+			customer.setTaxRates(taxrates);
+			inOrder.setCustomer(customer);
 			inOrder.setTaxes(cart.getTaxes());
-			inOrder.setSubTotal(cart.getSubTotal());
 			
 			ShippingMethod method = (ShippingMethod) store.getAllShippingMethods().get(0);
 			cart.setShippingMethod(method);
@@ -219,9 +252,9 @@ public class OrderSet {
 			cart.setBillingAddress(inOrder.getBillingAddress());
 			inOrder.setTotalShipping(cart.getTotalShipping());
 			
+			inOrder.setTotalTax(cart.getTotalTax());
 			inOrder.setSubTotal(cart.getSubTotal());
 			inOrder.setTotalPrice(cart.getTotalPrice());
-			inOrder.setTotalTax(cart.getTotalTax());
 		} else {
 			System.out.println("Order skipped. No Cart Items");
 		}
@@ -230,8 +263,28 @@ public class OrderSet {
 	public void recalculateAll( Store store ) {
 		for (Iterator<Order> iterator = getOrders().iterator(); iterator.hasNext();) {
 			Order order = (Order) iterator.next();
-			recalculateOrder(order, store);
+			if (order.getItems().size() > 0) {
+				recalculateOrder(order, store);
+			}
+		}
+		removeEmptyOrders();
+	}
+	public void removeEmptyOrders() {
+		List<Order> removeOrders = new ArrayList<Order>();
+		for (Iterator<Order> iterator = getOrders().iterator(); iterator.hasNext();) {
+			Order order = iterator.next();
+			if (order.getItems().size() == 0) {
+				removeOrders.add(order);
+				addRemovedOrder(order);
+				continue;
+			}
+		}
+		if (removeOrders.size() > 0) {
+			for (Iterator<Order> iterator = removeOrders.iterator(); iterator.hasNext();) {
+				Order order = (Order) iterator.next();
+				System.out.println("Order #" + order.getId() + " removed from OrderSet (no items) ");
+				getOrders().remove(order);
+			}
 		}
 	}
-	
 }
