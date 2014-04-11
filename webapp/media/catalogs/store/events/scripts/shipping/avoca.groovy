30ec3303085b5aa3befef4eaaf0c3cd7b41004d7
Shawn Best <shawn@ijsolutions.ca>
2014-04-11 13:47:48 -0400

    Avoca shipping costs

--- webapp/media/catalogs/store/events/scripts/shipping/avoca.groovy
+++ webapp/media/catalogs/store/events/scripts/shipping/avoca.groovy
@@ -1,74 +1,58 @@
 package shipping;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.dom4j.Element
 import org.openedit.Data
 import org.openedit.data.BaseData
 import org.openedit.money.Money
 import org.openedit.store.Cart
 import org.openedit.store.CartItem
+import org.openedit.store.modules.StoreSearchModule;
 import org.openedit.store.shipping.BaseShippingMethod
 
 public class avoca extends BaseShippingMethod {
+	
+	private static final Log log = LogFactory.getLog(avoca.class);
+	
 	protected String fieldDistributor = "106";
 	
 	public Money getCost(Cart inCart) {
-	
 		if(inCart.getShippingAddress() == null || inCart.getShippingAddress().getState() == null){
 			return new Money();
 		}
 		String state = inCart.getShippingAddress().getState();
-		switch (state) {
-		case "NL":
-			return new Money(18);
-			break;
-
-			case "NS":
-			return new Money(18);
-			break;
-			case "PE":
-			return new Money(18);
-			break;
-			case "NB":
-			return new Money(18);
-			break;
-			case "QC":
-			return new Money(10);
-			break;
-			case "ON":
-			return new Money(10);
-			break;
-			case "MB":
-			return new Money(16);
-			break;
-
-			case "SK":
-			return new Money(16);
-			break;
-			case "AB":
-			return new Money(14);
-			break;
-			case "BC":
-			return new Money(14);
-			break;
-			case "NT":
-			return new Money(20);
-			break;
-			case "NT":
-			return new Money(20);
-			break;
-			case "OTH":
-			return new Money(20);
-			break;
-			
-			
-			
-			
-		default:
+		if (state == null)
+		{
 			return new Money();
-			break;
 		}
-
-		
+		//maritimes 18
+		if (state.equals("NL") || state.equals("NS") || state.equals("PE") || state.equals("NB"))
+		{
+			return new Money(18);
+		}
+		//QC ON 10
+		if (state.equals("QC") || state.equals("ON"))
+		{
+			return new Money(10);
+		}
+		//MB SK 16
+		if (state.equals("MB") || state.equals("SK"))
+		{
+			return new Money(16);
+		}
+		//AB BC 14
+		if (state.equals("AB") || state.equals("BC"))
+		{
+			return new Money(14);
+		}
+		//YT NU 20
+		if (state.equals("YT") || state.equals("NU"))
+		{
+			return new Money(20);
+		}
+		log.info("Warning: unknown province = $state, returning 0");
+		return new Money();
 	}
 
 	public boolean applies(Cart inCart) {
@@ -85,6 +69,7 @@ public class avoca extends BaseShippingMethod {
 		// TODO Auto-generated method stub
 
 	}
+	
 	private Money getDistributorTotal(Cart inCart) {
 		Money totalformicrocel = new Money();
 		for (Iterator iterator = inCart.getItems().iterator(); iterator.hasNext();) {
@@ -97,13 +82,9 @@ public class avoca extends BaseShippingMethod {
 		}
 		return totalformicrocel;
 	}
-	public Collection getHints(Cart inCart){
-	ArrayList hints = new ArrayList();
 	
+	public Collection getHints(Cart inCart){
+		ArrayList hints = new ArrayList();
 		return hints;
 	}
-	
-	
-	
-	
 }
