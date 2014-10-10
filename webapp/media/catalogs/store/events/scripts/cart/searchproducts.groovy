15c65dba1ede175f8cf4613552edd15cd94f9b9a
Shawn Best <shawn@ijsolutions.ca>
2014-10-09 21:53:56 -0400

    search for products

--- webapp/media/catalogs/store/events/scripts/cart/searchproducts.groovy
+++ webapp/media/catalogs/store/events/scripts/cart/searchproducts.groovy
@@ -1,37 +1,10 @@
-package products
-
-import org.openedit.Data
 import org.openedit.data.Searcher
-import org.openedit.data.SearcherManager
 import org.openedit.entermedia.MediaArchive
-import org.openedit.util.DateStorageUtil
-import org.openedit.money.Money
-import org.openedit.store.CartItem
-import org.openedit.store.Price;
-import org.openedit.store.PriceSupport
-import org.openedit.store.PriceTier;
 import org.openedit.store.Store
-import org.openedit.store.Product
-import org.openedit.store.InventoryItem
-import org.openedit.store.orders.Order
-import org.openedit.store.util.MediaUtilities
-import org.openedit.store.orders.OrderArchive
-import org.openedit.store.orders.OrderId
-import org.openedit.store.orders.Shipment
-import org.openedit.store.orders.ShipmentEntry
-import org.openedit.store.orders.Refund
-import org.openedit.store.orders.RefundItem
-import org.openedit.store.orders.RefundState
 
 import com.openedit.WebPageRequest
 import com.openedit.hittracker.HitTracker
-import com.openedit.hittracker.ListHitTracker
 import com.openedit.hittracker.SearchQuery
-import com.openedit.page.Page
-import com.openedit.page.manage.PageManager
-import com.openedit.util.XmlUtil
-
-import groovy.util.slurpersupport.GPathResult
 
 
 public void init() {
@@ -76,13 +49,13 @@ public void init() {
 		childquery.addMatches("distributor",distributorid);
 		query.addChildQuery(childquery);
 	}
-	log.info("### running query: ${query.toFriendly()}");
 	int hitsperpage = toInt(inReq.findValue("hitsperpage"),9);
 	HitTracker hits = searcher.search(query);
-	log.info("### $hits")
-//	hits.setHitsPerPage(hitsperpage);
+	hits.setHitsPerPage(hitsperpage);
 	String hitsname = inReq.findValue("hitsname");
-	if (!hitsname) hitsname = "hits";
+	if (!hitsname){
+		hitsname = "hits";
+	}
 	inReq.putPageValue(hitsname, hits);
 	log.info("---- END Searching for Products ----");
 }
