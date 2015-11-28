98e9f63f6a0ac14335318c56498f2067649c5f3c
shawn <shawn@ijsolutions.ca>
2015-11-28 12:55:58 -0500

    overridden method to return either negative or positive discount values

--- src/org/openedit/store/adjustments/SecurityGroupAdjustment.java
+++ src/org/openedit/store/adjustments/SecurityGroupAdjustment.java
@@ -1,5 +1,25 @@
 package org.openedit.store.adjustments;
 
-public class SecurityGroupAdjustment extends FixedPriceAdjustment {
+import org.openedit.money.Money;
+import org.openedit.store.CartItem;
+import org.openedit.store.Coupon;
 
+public class SecurityGroupAdjustment extends FixedPriceAdjustment {
+	
+	@Override
+	public Money adjust(CartItem inItem) {
+		if (!Coupon.isCoupon(inItem))
+		{
+			if (getProductId()!=null && inItem.getProduct()!=null)
+			{
+				if ( inItem.getProduct().getId().equals(getProductId()) )
+				{
+					Money discount = getDiscount();
+					Money price = inItem.getYourPrice();
+					return price.subtract(discount);
+				}
+			}
+		}
+		return null;
+	}
 }
